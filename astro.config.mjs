3f4fcc4d13e3a3ea3a58fa11b16700f5a6a6303c
little_suns <little_suns@foxmail.com>
2026-08-16 18:05:45 +0800

    feat: add WebGPU ambient world

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,9 @@ import react from "@astrojs/react";
 export default defineConfig({
   site: "https://littlesun.space",
   integrations: [react()],
+  vite: {
+    build: {
+      chunkSizeWarningLimit: 750,
+    },
+  },
 });
